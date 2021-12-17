8c64582707f020957f6f4282facd2764dbe6fdfc
DianaSolisG <diana.solisg@gmail.com>
2021-12-16 23:09:51 -0500

    crud objetvios

--- models/proyecto/tipos.js
+++ models/proyecto/tipos.js
@@ -13,6 +13,21 @@ input crearObjetivo{
     tipo: Enum_TipoObjetivo!
 }
 
+input camposObjetivo{
+    descripcion:String!
+    tipo: Enum_TipoObjetivo!
+}
+
+input camposProyectos{
+    nombre: String
+    presupuesto: Float
+    fechaInicio: Date
+    fechaFin: Date
+    estado: Enum_EstadoProyecto
+    fase: Enum_FaseProyecto
+    lider: String
+}
+
 type Proyecto{
     _id:ID!
     nombre: String!
@@ -42,6 +57,27 @@ type Mutation{
         fase: Enum_FaseProyecto!
         lider: String!
         objetivos: [crearObjetivo]
+    ):Proyecto
+
+    editarProyecto(
+        _id:String!
+        campos:camposProyectos!
+    ):Proyecto
+
+    crearObjetivo(
+        idProyecto:String!,
+        campos:camposObjetivo!
+    ):Proyecto
+
+    editarObjetivo(
+        idProyecto:String!,
+        indexObjetivo:Int!,
+        campos: camposObjetivo!
+    ):Proyecto
+
+    eliminarObjetivo(
+        idProyecto:String!,
+        idObjetivo:String!
     ):Proyecto
 
 }
